780086f7859792dd0131b2d62224a2a88d53dfda
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2020-08-27 12:39:54 -0400

    Delete promedio.py

--- promedio.py
+++
@@ -1,6 +0,0 @@
-#Realizar un programa que solicite diez números, los almacene en un array y luego calcule el promedio de esos números.
-
-numeros = []
-for x in range (10):
-    numeros.append(int(input("Introduce un numero:")))
-print(sum(numeros)/int(len(numeros)))
