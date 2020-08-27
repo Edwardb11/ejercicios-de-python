85a7b1a88b20eb66c5d5d35deaa7c41728feca2b
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2020-08-27 12:33:02 -0400

    Add files via upload

---
+++ semana06/Comprension.py
@@ -0,0 +1,8 @@
+#Realizar un programa que rellene una lista con los números enteros comprendidos entre 7 y 21.
+
+numeros = []
+for x in range(7,22):
+    numeros.append(x)
+    print(numeros[:])
+    
+    
