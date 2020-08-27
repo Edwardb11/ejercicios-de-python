87500f339b50d74e77001e8524d34cdd531866f8
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2020-08-27 12:40:20 -0400

    Delete max.py

--- max.py
+++
@@ -1,7 +0,0 @@
-#Realiza un programa que complete una lista con 10 elementos numericos, luego evaluar cual es el valor mas alto encontrado en dicha lista. 
-
-lista = []
-for a in range (1,11):
-    lista.append (a)
-print(max(lista))    
-
