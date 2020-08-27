f4457fea50564a78cfd2793944e0c06a286e49df
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2020-08-27 12:33:38 -0400

    Add files via upload

---
+++ Abecedario.py
@@ -0,0 +1,7 @@
+#Escribir un programa que almacene el abecedario en una lista, elimine de la lista las letras que ocupen posiciones múltiplos de 3, y muestre por pantalla la lista resultante.
+
+Abecedario = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+for a in range(len(Abecedario), 1, -1):
+    if a % 3 == 0:
+        Abecedario.pop(a-1)
+print(Abecedario)
